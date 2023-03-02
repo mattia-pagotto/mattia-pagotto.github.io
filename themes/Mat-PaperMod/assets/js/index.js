cf9e9ea4e4275231bc1bc8739e94344e3f152db9
mattia-pagotto <pag8mattia@gmail.com>
2023-03-02 23:04:43 +0100

    Add Google Tag Manager

---
+++ themes/Mat-PaperMod/assets/js/index.js
@@ -0,0 +1,8 @@
+var doNotTrack = !1;
+if (!doNotTrack) {
+	window.dataLayer = window.dataLayer || [];
+	function gtag() {
+		dataLayer.push(arguments);
+	}
+	gtag("js", new Date()), gtag("config", "G-PZ7TCJB9Y4", { anonymize_ip: !1 });
+}
